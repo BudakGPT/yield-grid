db5fc5a1c193e8f42c7800e66432e8428f4cf113
Haekal Handiran <haekal.handrian@ui.ac.id>
2026-07-17 03:35:49 +0700

    chore(infra): align backend runtime configuration

--- backend/src/main/java/budakgpt/yieldgridbackend/YieldgridBackendApplication.java
+++ backend/src/main/java/budakgpt/yieldgridbackend/YieldgridBackendApplication.java
@@ -2,8 +2,12 @@ package budakgpt.yieldgridbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import budakgpt.yieldgridbackend.config.IntegrationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(IntegrationProperties.class)
 public class YieldgridBackendApplication {
 
     public static void main(String[] args) {
@@ -11,4 +15,3 @@ public class YieldgridBackendApplication {
     }
 
 }
-    
